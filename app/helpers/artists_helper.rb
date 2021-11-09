9d265784348554525941b6a9fcee2f7d2f78d71d
amy-chinlai <amy@actionnetwork.org>
2021-11-09 13:17:19 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+
+    def display_artist(input)
+        artist = Song.find(input.id).artist
+        if artist != "" && artist
+            link_to artist.name, artist_path(artist)
+        else
+            link_to "Add Artist", edit_song_path(input)
+        end
+
+    end
 end
